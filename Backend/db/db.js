6eefbe623f85739148643298de8327b41ad2ad91
Nisha Panda <159682094+NishaPanda@users.noreply.github.com>
2025-12-30 23:54:56 +0530

    Login error solved

--- Backend/db/db.js
+++ Backend/db/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const MONGO_URI = process.env.MONGODB_URI;
+const MONGO_URI = process.env.MONGO_URI;
 function connectDB() {
     mongoose.connect(MONGO_URI)
         .then(() => {
